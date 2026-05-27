8560da38b5fe77e7ea1e22bd481db53446487d87
Kai Kameyama <1203kk06@gmail.com>
2026-05-27 13:26:58 +0800

    init

---
+++ packages/ui/src/components/skeleton.tsx
@@ -0,0 +1,13 @@
+import { cn } from "@kursa/ui/lib/utils";
+
+function Skeleton({ className, ...props }: React.ComponentProps<"div">) {
+  return (
+    <div
+      data-slot="skeleton"
+      className={cn("animate-pulse rounded-none bg-muted", className)}
+      {...props}
+    />
+  );
+}
+
+export { Skeleton };
