3885cf2a6ee9d0fe762078ce4aab648e99fbfa39
Kai Kameyama <1203kk06@gmail.com>
2026-05-31 17:33:55 +0800

    imp/add PATCH in CORS

--- apps/server/src/index.ts
+++ apps/server/src/index.ts
@@ -18,7 +18,7 @@ app.use(helmet());
 app.use(
   cors({
     origin: env.CORS_ORIGIN,
-    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization"],
     credentials: true,
   }),
